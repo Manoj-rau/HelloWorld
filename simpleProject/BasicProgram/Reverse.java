5cef45478cf7e9fd99696e9a829087f2ffee2d9d
hp <hp@DESKTOP-NMJI90H>
2019-11-02 19:37:42 +0530

    Code written in Reverse

--- simpleProject/BasicProgram/Reverse.java
+++ simpleProject/BasicProgram/Reverse.java
@@ -1,5 +1,20 @@
 package BasicProgram;
 
 public class Reverse {
+	
+	public static void main(String[] args) {
+		int x=252;
+		int y=0;
+		int z;
+		
+		while(x != 0) 
+		{
+            z = x % 10;
+            y = y * 10 + z;
+            x /= 10;
+        }
+        System.out.println("Reversed Number: " + y);
+		
+	}
 
 }
